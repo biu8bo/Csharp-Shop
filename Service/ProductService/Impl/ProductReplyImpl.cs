122d2c674b590e4618d91054d7cac77b22b67275
xiaoxiao <2738644273@QQ.com>
2022-02-15 22:35:22 +0800

    优化商品评论接口

--- Service/ProductService/Impl/ProductReplyImpl.cs
+++ Service/ProductService/Impl/ProductReplyImpl.cs
@@ -1,8 +1,10 @@
 ﻿using Commons.BaseModels;
 using Mapper;
 using MVC卓越项目.Commons.Utils;
+using Service.ProductService.VO;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +13,26 @@ namespace Service.Service
 {
     public class ProductReplyImpl : IProductReply
     {
-        public PageModel GetReplyByPid(long pid,int page,int limit)
+        public PageModel GetReplyByPid(long pid, int page, int limit)
         {
             using (var db = new eshoppingEntities())
             {
-                return new PageUtils<store_product_reply>(page,limit).StartPage(db.store_product_reply.Where(e => e.product_id == pid && e.is_del == false).OrderBy(e=>e.id));
+                return new PageUtils<Object>(page, limit).StartPage(db.store_product_reply.Where(e => e.product_id == pid && e.is_del == false).Join(
+                    db.eshop_user,  //外部对象
+                    reply => reply.uid,         //内部的key
+                user => user.uid,         //外部的key
+                (reply, user) => new        //结果
+                {
+                    id = reply.id,
+                    comment = reply.comment,
+                    username = user.username,
+                    nickname = user.nickname,
+                    createTime  = reply.create_time,
+                    isReply = reply.is_reply,
+                    productScore = reply.product_score,
+                    serviceScore = reply.service_score,
+                    pics = reply.pics
+                }).OrderBy(e=>e.id));
             }
         }
     }
---
+++ Service/ProductService/VO/ProductReply.cs
@@ -0,0 +1,33 @@
+﻿using Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ProductService.VO
+{
+   public class ProductReply
+    {
+        public long id { get; set; }
+        public long uid { get; set; }
+        public long oid { get; set; }
+        public string unique { get; set; }
+        public long product_id { get; set; }
+        public string reply_type { get; set; }
+        public bool product_score { get; set; }
+        public bool service_score { get; set; }
+        public string comment { get; set; }
+        public string pics { get; set; }
+        public System.DateTime create_time { get; set; }
+        public Nullable<System.DateTime> update_time { get; set; }
+        public string merchant_reply_content { get; set; }
+        public Nullable<System.DateTime> merchant_reply_time { get; set; }
+        public bool is_del { get; set; }
+        public bool is_reply { get; set; }
+
+        public string username { get; set; }
+        public string nickname { get; set; }
+        public string avatar { get; set; }
+    }
+}
